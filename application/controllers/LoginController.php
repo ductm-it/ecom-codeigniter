ec34511fe412162810f1267d12b089ec34d0def4
ductm-it <duc.tran@southtelecom.vn>
2022-09-13 00:19:11 +0700

    create login

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -3,6 +3,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class LoginController extends CI_Controller
 {
+    public function __construct(){
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->model('LoginModel');
+    }
     public function index()
     {
         $this->load->view('template/header');
@@ -11,6 +16,28 @@ class LoginController extends CI_Controller
     }
 
     public function login(){
-        
+        $this->form_validation->set_rules('email', 'Email', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required', ['required' => 'You must provide a %s.']);
+        if ($this->form_validation->run()) {
+            $email = $this->input->post('email');
+            $password = md5($this->input->post('password'));
+            $result = $this->LoginModel->checkLogin($email, $password);
+            if($result) {
+                $session_array = [
+                    'id' => $result[0]->id,
+                    'username' => $result[0]->username,
+                    'email' => $result[0]->email
+                ];
+                $this->session->set_userdata('LoggedIn',$session_array);
+                $this->session->set_flashdata('success', 'Login successful');
+                redirect(base_url('/'));
+            } else {
+                $this->session->set_flashdata('error', 'Login fail');
+                redirect(base_url('login'));
+
+            }
+        } else {
+            $this->index();
+        }
     }
 }
--- application/views/login/index.php
+++ application/views/login/index.php
@@ -1,15 +1,29 @@
 <div class="container">
+    <?php
+    if ($this->session->flashdata('success')) {
+        ?>
+        <div class="alert alert-success"><?php echo $this->session->flashdata('success') ?></div>
+    <?php
+    } elseif ($this->session->flashdata('error')) {
+        ?>
+        <div class="alert alert-danger"><?php echo $this->session->flashdata('error') ?></div>
+    <?php
+        }
+    ?>
+    <h1>Login Page</h1>
     <form action="<?php echo base_url('login-user'); ?>" method="POST">
         <!-- Email input -->
         <div class="form-outline mb-4">
             <input type="email" name="email" class="form-control" placeholder="Email" id="form2Example1" class="form-control" />
             <label class="form-label" for="form2Example1">Email address</label>
+            <?php echo form_error('email'); ?>
         </div>
 
         <!-- Password input -->
         <div class="form-outline mb-4">
             <input type="password" name="password" class="form-control" placeholder="Password" id="form2Example2" class="form-control" />
             <label class="form-label" for="form2Example2">Password</label>
+            <?php echo form_error('password'); ?>
         </div>
 
         <!-- 2 column grid layout for inline styling -->
